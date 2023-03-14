18ab6215a0a282f266291a99121a187927de16d3
De65 <kyalodiana6@gmail.com>
2023-03-14 23:59:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log (multiply)
 
-// Write your code here
+let int = 10
+let random =int
+console.log (random)
+
+let num3 = 22
+let num4 = 6
+let mob = 4
+console.log(mob)
+
+let max = (2,5,6,10,20)
+console.log (max)
+
