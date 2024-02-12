44848118557b7c93cea533a039a8f7f0a1f8afe4
De65 <kyalodiana6@gmail.com>
2024-02-13 00:36:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-let num1 = 31
-let num2 = 2
-let multiply = num1 * num2
-console.log (multiply)
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-let int = 10
-let random =int
-console.log (random)
+// let int = 10;
+// let random = int;
+// console.log(random);
+let num = 10;
+let random = Math.floor(Math.random() * num) + 1;
+console.log(random);
 
-let num3 = 22
-let num4 = 6
-let mod = num3 % num4
-console.log(mod)
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
 
-let max = (2,5,6,10,20)
-console.log (max)
-
+// let max = (2, 5, 6, 10, 20);
+// console.log(max);
+let max = Math.max(2, 5, 6, 10, 20);
+console.log(max);
